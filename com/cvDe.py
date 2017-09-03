a8b03cfd70e2f36a39c1a6dd34b3344feaa634be
hhzh <1138253815@qq.com>
2017-09-03 22:26:59 +0800

    reformat

--- com/cvDe.py
+++ com/cvDe.py
@@ -4,4 +4,4 @@ import pytesseract
 # img = Image.open('e:/hello.png')
 # img.load()
 # img.show()
-print(pytesseract.image_to_string(Image.open('e:/en.png'),lang='eng'))
+print(pytesseract.image_to_string(Image.open('e:/en.png'), lang='eng'))

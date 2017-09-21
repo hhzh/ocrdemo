272bcb4937fb681202862f6d38ee7e11bb6ce3eb
hhzh <1138253815@qq.com>
2017-09-21 18:26:40 +0800

    update

--- img/uploadm2.py
+++ img/uploadm2.py
@@ -46,7 +46,7 @@ def upload_MD5():
                 logging.info('创建目录：%s', os.path.join(os.path.abspath('./img'), imgMD5))
             with open('./img/' + imgMD5 + '/upload.jpg', 'wb') as fp:
                 fp.write(response.content)
-            return end_process(imgMD5, userId, caseType)
+            return cv_img(imgMD5, userId, caseType)
 
 
 @app.route('/upload', methods=['POST'])
@@ -68,22 +68,19 @@ def upload_file():
             for line in lines:
                 if line.startswith('<h1>MD5'):
                     imgMD5 = line[line.index('<h1>MD5:') + 8:line.index('</h1>')].strip()
-                    if not os.path.exists(os.path.join(os.path.abspath('./img'), imgMD5)):
-                        os.makedirs(os.path.join(os.path.abspath('./img'), imgMD5))
-                        logging.info('创建目录:%s', os.path.join(os.path.abspath('./img'), imgMD5))
-                    with open('./img/' + imgMD5 + '/upload.jpg', 'wb') as fp:
-                        fp.write(open('./img/img.jpg', 'rb').read())
                     logging.info('上传图片 %s 成功！', imgMD5)
-                    return end_process(imgMD5, userId, caseType)
+                    return cv_img(imgMD5, userId, caseType)
 
         return 'ocr error.'
 
 
-def cv_img(imgMD5, caseType):
-    afile = os.path.join(os.path.abspath('./img/' + imgMD5), 'upload.jpg')
+def cv_img(imgMD5, userId, caseType):
+    afile = os.path.join(os.path.abspath('./img'), 'img.jpg')
     filepath, filename = os.path.split(afile)
 
     img = cv2.imread(os.path.join(filepath, filename))
+
+    images = []
 
     try:
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -104,7 +101,6 @@ def cv_img(imgMD5, caseType):
         # c = sorted(contours, key=cv2.contourArea, reverse=True)
         c = sorted(contours, key=lambda con: min(con[:, 0, 1]))
 
-        mm = 1
         for cc in c:
             # compute the rotated bounding box of the largest contour
             rect = cv2.minAreaRect(cc)
@@ -135,32 +131,17 @@ def cv_img(imgMD5, caseType):
                 M = cv2.getPerspectiveTransform(pts1, pts2)
                 cropImg = cv2.warpPerspective(img, M, (width, height))
 
-            cv2.imwrite(filepath + '/' + str(mm) + 'img.jpg', cropImg)
+            # cv2.imwrite(filepath + '/' + str(mm) + 'img.jpg', cropImg)
+            images.append(cropImg)
 
-            cv2.drawContours(img, [box], -1, (0, 255, 0), 3)
-            mm = mm + 1
-        cv2.imwrite(os.path.join(filepath, 'zresult.jpg'), img)
+            # cv2.drawContours(img, [box], -1, (0, 255, 0), 3)
+            # cv2.imwrite(os.path.join(filepath, 'zresult.jpg'), img)
     except:
-        logging.error('图片处理出错：%s', afile)
+        logging.error('图片处理出错：%s', imgMD5)
+    begin_ocr(imgMD5, images, userId, caseType)
 
 
-# 读取图片
-def get_file_content(filepath):
-    with open(filepath, 'rb') as fp:
-        return fp.read()
-
-
-def traverse(filepath):
-    files = os.listdir(filepath)
-    for fi in files:
-        fi_d = os.path.join(filepath, fi)
-        if os.path.isdir(fi_d):
-            traverse(fi_d)
-        elif fi_d.endswith('img.jpg'):
-            paths.append(os.path.join(filepath, fi_d))
-
-
-def begin_ocr(imgMD5):
+def begin_ocr(imgMD5, images, userId, caseType):
     # vip
     APP_ID = '10073324'
     API_KEY = 'zapROApDpKIY2xGF4LXwUTj4'
@@ -168,45 +149,26 @@ def begin_ocr(imgMD5):
 
     aipOcr = AipOcr(APP_ID, API_KEY, SECRET_KEY)
 
-    traverse(os.path.join(os.path.abspath('./img'), imgMD5))
+    result_txt = ''
 
-    paths1 = set(paths)
-
-    paths2 = list(paths1)
-
-    paths2.sort()
-
-    logging.info('paths2 is: %s', paths2)
-
-    for imgpath in paths2:
-        path1, name = os.path.split(imgpath)
-        logging.info('开始处理:%s', imgpath)
-
+    for i, sub_img in enumerate(images):
         try:
-            result = aipOcr.basicGeneral(get_file_content(imgpath))
+            result = aipOcr.basicGeneral(sub_img)
             if result.get('words_result_num', 0) > 0:
                 for obj in result['words_result']:
-                    with open(os.path.join(path1, 'result.txt'), 'a', encoding='utf-8') as fp:
-                        fp.write(obj['words'])
-                        fp.write('\n')
+                    result_txt += obj['words']
+                    result_txt += '\n'
             else:
-                logging.warning('图片 %s 未识别：%s', imgpath, str(result))
+                logging.warning('图片 %s 未识别：%s', imgMD5 + '/' + str(i), str(result))
         except Exception as e:
-            logging.error('识别图片 %s 出错：%s', imgpath, str(e))
+            logging.error('识别图片 %s 出错：%s', imgMD5, str(e))
+    end_process(imgMD5, userId, caseType, result_txt)
 
 
-def end_process(imgMD5, userId, caseType):
-    cv_img(imgMD5, caseType)
-    begin_ocr(imgMD5)
-
-    if not os.path.exists(os.path.join(os.path.abspath('./img/' + imgMD5), 'result.txt')):
+def end_process(imgMD5, userId, caseType, result_txt):
+    if result_txt == '':
         return 'ocr error.'
     else:
-        result = ''
-        with open(os.path.join(os.path.abspath('./img/' + imgMD5), 'result.txt'), 'r',
-                  encoding='utf-8') as fp:
-            for line in fp.readlines():
-                result = result + line
         conn = pymysql.connect(host='114.112.104.149', port=3306, user='root', passwd='SCMD_2017_scmd', db='scmd',
                                charset='utf8')
         cursor = conn.cursor()
@@ -214,48 +176,20 @@ def end_process(imgMD5, userId, caseType):
         try:
             if userId is not None and userId != '' and userId != ' ':
                 sql = 'insert into sc_ocr (userId,imgMD5,result) values (%s,%s,%s)'
-                exec_result = cursor.execute(sql, (userId, imgMD5, result))
+                exec_result = cursor.execute(sql, (userId, imgMD5, result_txt))
             else:
                 sql = 'insert into sc_ocr (imgMD5,result) values (%s,%s)'
-                exec_result = cursor.execute(sql, (imgMD5, result))
+                exec_result = cursor.execute(sql, (imgMD5, result_txt))
         except Exception as e:
             logging.error('插入数据库出错：imgMd5=%s, userId=%s, caseType=%s, %s', imgMD5, userId, caseType, e)
         conn.commit()
         conn.close()
-        del_file(imgMD5)
         # return 'OK'
         # return jsonify({'result': result})
         # return jsonify({'imgMD5': imgMD5})
-        return result
+        return result_txt
         # return jsonify({'imgMD5': imgMD5, 'result': result})
 
-
-def del_file(imgMD5):
-    md5path = os.path.join(os.path.abspath('./img/' + imgMD5))
-    logging.info('删除目录：%s', md5path)
-    shutil.rmtree(md5path)
-
-
-# def invoke_cut(imgMD5, userId, result):
-#     requestobj = ocrrequest_pb2.Request()
-#     requestobj.Request_buf = result.encode('gbk')
-#     requestobj.UserId = 32
-
-#     requestobj.imgMD5 = '95083e7bca6b09fb4c02e7cd666ab506'.encode('gbk')
-#
-#     data = requestobj.SerializeToString()
-#     xhead = '\t' + 'QS55AACA'
-#     xlen = str(len(xhead) + len(data)).zfill(8)
-#     request_head = 'QSEpsL01QSBEAACA' + xlen + '\t'
-#     request_end = 'QS55AACA'
-#
-#     ip_port = ('114.112.104.150', 10001)
-#     web = socket.socket()
-#
-#     web.connect(ip_port)
-#     web.sendall(bytes(request_head, 'gbk') + data + bytes(request_end, 'gbk'))
-#     server_reply = web.recv(1024)
-#     web.close()
 
 
 if __name__ == '__main__':
